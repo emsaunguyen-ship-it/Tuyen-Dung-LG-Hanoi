2c24046241ba4e00d758dfe853d40a80dad5037e
THUY KHANH NGUYEN/Team Leader/LGEVH NORTH GENERAL  PROCUREMENT <khanhthuy.nguyen@lge.com>
2026-08-14 10:06:54 +0700

    Optimize page load speed: split 37MB bundle into lightweight index HTML (480B) and async assets

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { viteSingleFile } from 'vite-plugin-singlefile'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), viteSingleFile()],
+  base: './',
+  plugins: [react()],
   build: {
-    outDir: 'docs'
+    outDir: 'docs',
+    emptyOutDir: true
   }
 })
